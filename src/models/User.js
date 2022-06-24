26705e1f4913e86195e1bc2b8ed2f3ec8f72d47d
Radomir Petkov <105939480+RadomirPetkov@users.noreply.github.com>
2022-06-24 13:40:19 +0300

    add resources

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,15 @@
 const mongoose = require(`mongoose`)
 
 const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
     username: {
         type: String,
         required: [true, 'Username is required']
     },
     password: {
-        type: String,
-        required: true
-    },
-    adress: {
         type: String,
         required: true
     }
